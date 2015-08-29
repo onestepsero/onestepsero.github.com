93677729055b55e65219d148c1b4ed23f2916fca
onestepsero <rodlsdlf1>
2015-08-30 01:41:14 +0900

    [Haedal] 주소록 js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,156 @@
-console.log('This would be the main JS file.');
+var frequency_list10 = [{"text": "대빵", "size": 40}, {"text": "완두콩", "size": 15}
+    , {"text": "하마", "size": 20}, {"text": "기린", "size": 25}
+    , {"text": "소금", "size": 30} , {"text": "노을", "size": 35}
+    , {"text": "햇님", "size": 40}, {"text": "달님", "size": 15}
+    , {"text": "부엉이", "size": 20}, {"text": "올빼미", "size": 25}
+    , {"text": "장풍", "size": 30} , {"text": "홍시", "size": 35}
+    , {"text": "다같이", "size": 20}, {"text": "언제나", "size": 25}
+    , {"text": "달팽이", "size": 20}, {"text": "롤라", "size": 25}
+    , {"text": "개복치", "size": 30} , {"text": "흰동가리", "size": 35}
+    , {"text": "거북이", "size": 30} , {"text": "눈토끼", "size": 35}
+];
+
+var frequency_list09 = [{"text": "팝콘", "size": 40}, {"text": "강냉이", "size": 15}
+    , {"text": "우주", "size": 20}, {"text": "즐거워", "size": 25}
+    , {"text": "나무꾼", "size": 30}, {"text": "구름빵", "size": 35}
+    , {"text": "감나무", "size": 40}, {"text": "메이", "size": 15}
+    , {"text": "백곰", "size": 20}, {"text": "하루", "size": 25}
+    , {"text": "연필", "size": 30}, {"text": "지우개", "size": 35}
+    , {"text": "마당", "size": 30}, {"text": "단비", "size": 35}
+
+];
+
+var frequency_list08 = [{"text": "몰라", "size": 40}, {"text": "진주", "size": 15}
+    , {"text": "김밥", "size": 20}, {"text": "선물", "size": 25}
+    , {"text": "뭉치", "size": 30} , {"text": "샛별", "size": 35}
+    , {"text": "감자", "size": 40}, {"text": "당근", "size": 15}
+    , {"text": "개복치", "size": 20}, {"text": "흰동가리", "size": 25}
+    , {"text": "차돌", "size": 30} , {"text": "풀", "size": 35}
+    , {"text": "네모네", "size": 20}, {"text": "수수꽃다리", "size": 25}
+];
+
+
+var frequency_list07 = [{"text": "구기자", "size": 40}, {"text": "보리수", "size": 15}
+    , {"text": "클", "size": 20}, {"text": "노란지붕", "size": 25}
+    , {"text": "소금", "size": 30} , {"text": "노을", "size": 35}
+    , {"text": "동동", "size": 40}, {"text": "아리아리", "size": 15}
+    , {"text": "고구마", "size": 20}, {"text": "홍차", "size": 25}
+    , {"text": "농부", "size": 30} , {"text": "딸기", "size": 35}
+    , {"text": "도깨비", "size": 20}, {"text": "송글", "size": 25}
+    , {"text": "길목", "size": 20}, {"text": "봄볕", "size": 25}
+    , {"text": "야호", "size": 30} , {"text": "제제", "size": 35}
+    , {"text": "희망", "size": 20}, {"text": "여름밤", "size": 25}
+    , {"text": "낙엽", "size": 30} , {"text": "이파리", "size": 35}
+];
+
+
+var frequency_list06 = [{"text": "항아리", "size": 40}, {"text": "연두", "size": 15}
+    , {"text": "산울림", "size": 20}, {"text": "메아리", "size": 25}
+    , {"text": "마늘", "size": 30} , {"text": "사과", "size": 35}
+    , {"text": "오로라", "size": 40}, {"text": "떡볶이", "size": 15}
+    , {"text": "버들치", "size": 20}, {"text": "이팝나무", "size": 25}
+    , {"text": "다같이", "size": 30} , {"text": "언제나", "size": 35}
+    , {"text": "마당", "size": 20}, {"text": "단비", "size": 25}
+    , {"text": "고슴도치", "size": 20}, {"text": "밍몽", "size": 25}
+    , {"text": "솟대", "size": 30} , {"text": "맷돌", "size": 35}
+];
+
+
+
+var frequency_list05 = [{"text": "기차", "size": 40}, {"text": "꽃게", "size": 15}
+    , {"text": "웃는곰", "size": 20}, {"text": "햇살아래", "size": 25}
+    , {"text": "바다", "size": 30} , {"text": "하늘", "size": 35}
+    , {"text": "아톰", "size": 40}, {"text": "달팽이", "size": 15}
+    , {"text": "김밥", "size": 20}, {"text": "선물", "size": 25}
+    , {"text": "곰돌이푸", "size": 30} , {"text": "꿀단지", "size": 35}
+    , {"text": "솟대", "size": 20}, {"text": "맷돌", "size": 25}
+    , {"text": "똑똑박사", "size": 20}, {"text": "착한이", "size": 25}
+    , {"text": "은하수", "size": 30} , {"text": "아기냄새", "size": 35}
+    , {"text": "니모", "size": 30} , {"text": "도리", "size": 35}
+];
+
+
+var frequency_list04 = [{"text": "도깨비", "size": 40}, {"text": "송글", "size": 15}
+    , {"text": "구기자", "size": 20}, {"text": "보리수", "size": 25}
+    , {"text": "항아리", "size": 30} , {"text": "연두", "size": 35}
+    , {"text": "네모네", "size": 40}, {"text": "수수꽃다리", "size": 15}
+    , {"text": "날개", "size": 20}, {"text": "안녕무지개", "size": 25}
+    , {"text": "해바라기", "size": 30} , {"text": "나비똥", "size": 35}
+    , {"text": "돌고래", "size": 20}, {"text": "파랑새", "size": 25}
+    , {"text": "버들치", "size": 20}, {"text": "이팝나무", "size": 25}
+    , {"text": "소나기", "size": 30} , {"text": "마루", "size": 35}
+    , {"text": "우물", "size": 30} , {"text": "딱따구리", "size": 35}
+];
+
+
+var frequency_list03 = [{"text": "구름", "size": 40}, {"text": "바람", "size": 15}
+    , {"text": "부엉이", "size": 20}, {"text": "올빼미", "size": 25}
+    , {"text": "잔디", "size": 30} , {"text": "썰매", "size": 35}
+    , {"text": "냉긋", "size": 40}, {"text": "달똥달", "size": 15}
+    , {"text": "아주", "size": 20}, {"text": "조아", "size": 25}
+    , {"text": "마당", "size": 30} , {"text": "뽀글이", "size": 35}
+    , {"text": "산울림", "size": 20}, {"text": "메아리", "size": 25}
+    , {"text": "강물", "size": 20}, {"text": "샘물", "size": 25}
+
+];
+
+
+
+var frequency_list02 = [{"text": "솜사탕", "size": 40}, {"text": "감자", "size": 15}
+    , {"text": "나무", "size": 20}, {"text": "잎싹", "size": 25}
+    , {"text": "바다", "size": 30} , {"text": "하늘", "size": 35}
+    , {"text": "솔로몬", "size": 40}, {"text": "민들레", "size": 15}
+    , {"text": "메뚜기", "size": 20}, {"text": "패랭이", "size": 25}
+    , {"text": "마늘", "size": 30} , {"text": "사과", "size": 35}
+    , {"text": "은하수", "size": 20}, {"text": "아기냄새", "size": 25}
+    , {"text": "아주", "size": 20}, {"text": "조아", "size": 25}
+    , {"text": "해바라기", "size": 30} , {"text": "나비똥", "size": 35}
+    , {"text": "울타리", "size": 30} , {"text": "새새", "size": 35}
+];
+
+
+var frequency_list01 = [{"text": "솜사탕", "size": 40}, {"text": "감자", "size": 15}
+    , {"text": "솔로몬", "size": 20}, {"text": "민들레", "size": 25}
+    , {"text": "염소", "size": 35}
+    , {"text": "들마루", "size": 15}
+    , {"text": "꼼지", "size": 25}
+    , {"text": "도토리", "size": 35}
+    , {"text": "옹달샘", "size": 25}
+    , {"text": "별님", "size": 25}
+    , {"text": "잔디", "size": 30} , {"text": "썰매", "size": 35}
+
+];
+
+
+var frequency_list_move = [{"text": "초록연", "size": 40}, {"text": "은밤", "size": 15}
+    , {"text": "구름", "size": 20}, {"text": "바람", "size": 25}
+    , {"text": "고구마", "size": 30} , {"text": "홍차", "size": 35}
+    , {"text": "뒷북", "size": 40}, {"text": "하늬", "size": 15}
+    , {"text": "까치", "size": 20}, {"text": "파란숲", "size": 25}
+    , {"text": "고든", "size": 30} , {"text": "달님", "size": 35}
+    , {"text": "금붕어", "size": 20}, {"text": "소나무", "size": 25}
+    , {"text": "소나기", "size": 20}, {"text": "마루", "size": 25}
+    , {"text": "들풀", "size": 30} , {"text": "들꽃", "size": 35}
+    , {"text": "영맨", "size": 30} , {"text": "코스모스", "size": 35}
+    , {"text": "낙엽", "size": 20}, {"text": "이파리", "size": 25}
+    , {"text": "아깨비", "size": 30} , {"text": "거울", "size": 35}
+    , {"text": "당나귀", "size": 30} , {"text": "콩", "size": 35}
+    , {"text": "도사", "size": 30} , {"text": "반디", "size": 35}
+];
+
+
+var frequency_list_boon = [{"text": "조랑말", "size": 40}, {"text": "나비", "size": 15}
+    , {"text": "바위", "size": 20}, {"text": "이슬", "size": 25}
+    , {"text": "팽이", "size": 30} , {"text": "파랑새", "size": 35}
+    , {"text": "돌고래", "size": 40}, {"text": "꼬꼬", "size": 15}
+    , {"text": "코끼리", "size": 20}, {"text": "앵두", "size": 25}
+];
+
+
+
+var frequency_list_teacher = [{"text": "진달래", "size": 40}, {"text": "눈사람", "size": 15}
+    , {"text": "메뚜기", "size": 20}, {"text": "토마토", "size": 25}
+    , {"text": "토토로", "size": 30} , {"text": "소금꽃", "size": 35}
+    , {"text": "찹쌀떡", "size": 40} , {"text": "봄날", "size": 35}
+    , {"text": "크레파스", "size": 30} , {"text": "산토끼", "size": 35}, {"text": "별님", "size": 35}
+];
